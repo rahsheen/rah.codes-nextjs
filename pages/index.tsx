5ee44c2ce70987af2e38b835bc58017cfa056b35
Rahsheen Porter <rahsheen.porter@gmail.com>
2020-08-24 23:53:20 -0400

    Add YouTube Embed

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,23 @@ import { Heading, Box } from "@chakra-ui/core";
 
 export default function Home() {
   return (
-    <Box p={[2,4,6,8]}>
+    <Box p={[2, 4, 6, 8]}>
       <Head>
         <title>rah.codes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box>
         <Heading>rah.codes</Heading>
+        <Box d="flex" justifyContent="center" alignItems="center">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/0eJ7WacNuE8"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </Box>
         <Query query={ARTICLES_QUERY}>
           {({ data: { articles } }) => {
             return <Articles articles={articles} />;
