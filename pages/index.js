17ecad18b385fa816256b5216f9563addba7ac62
Rahsheen Porter <rahsheen.porter@gmail.com>
2020-08-25 22:38:41 -0400

    Reset this biatch

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Container from "@/components/container";
+import MoreStories from "@/components/more-stories";
+import HeroPost from "@/components/hero-post";
+import Intro from "@/components/intro";
+import Layout from "@/components/layout";
+import { getAllPostsForHome } from "@/lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "@/lib/constants";
+
+export default function Index({ allPosts, preview }) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <>
+      <Layout preview={preview}>
+        <Head>
+          <title>Rah.codes</title>
+        </Head>
+        <Container>
+          <Intro />
+
+          <section className="mb-8 md:mb-16 flex justify-center">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/0eJ7WacNuE8"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </section>
+
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps({ preview = null }) {
+  const allPosts = (await getAllPostsForHome(preview)) || [{}];
+  console.log("POOP", allPosts);
+  return {
+    props: { allPosts, preview },
+  };
+}
