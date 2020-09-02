6305ae955ab52be540c9e9402d5d8998eaa1e3ca
Rahsheen Porter <rahsheen.porter@gmail.com>
2020-09-01 22:43:08 -0400

    Add latest YouTube video
    - implement incremental static revalidation

--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,34 @@ import Intro from "@/components/intro";
 import Layout from "@/components/layout";
 import { getAllPostsForHome } from "@/lib/api";
 import Head from "next/head";
-import { CMS_NAME } from "@/lib/constants";
+import { SITE_NAME } from "@/lib/constants";
+import { getLatestVideos } from "../lib/api";
 
-export default function Index({ allPosts, preview }) {
+export default function Index({ allPosts, preview, latestVideos }) {
   const heroPost = allPosts[0];
   const morePosts = allPosts.slice(1);
+
   return (
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Rah.codes</title>
+          <title>{SITE_NAME}</title>
         </Head>
         <Container>
           <Intro />
 
           <section className="mb-8 md:mb-16 flex justify-center">
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/0eJ7WacNuE8"
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
+            {latestVideos[0] && (
+              <iframe
+                width="560"
+                height="315"
+                title={latestVideos[0].snippet.title}
+                src={`https://www.youtube.com/embed/${latestVideos[0].id.videoId}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            )}
           </section>
 
           {heroPost && (
@@ -49,8 +54,18 @@ export default function Index({ allPosts, preview }) {
 
 export async function getStaticProps({ preview = null }) {
   const allPosts = (await getAllPostsForHome(preview)) || [];
-  
+
+  const latestVideos = (await getLatestVideos()) || [];
+
   return {
-    props: { allPosts, preview },
+    props: {
+      allPosts,
+      preview,
+      latestVideos,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every second
+    revalidate: 86400, // In seconds,
   };
 }
